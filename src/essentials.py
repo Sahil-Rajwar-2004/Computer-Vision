022d2f1dd14b4a10abf7c0fa55ebab1eb53fd2a5
Sahil-Rajwar <sahilrajwar2004@gmail.com>
2023-08-16 22:07:54 +0530

    essentials.py added

--- src/essentials.py
+++ src/essentials.py
@@ -1,4 +1,5 @@
 from tkinter import filedialog
+import numpy as np
 import cv2
 import os
 
@@ -9,23 +10,27 @@ absPATH = os.path.join(mainPATH,filePATH)
 fullPATH = filedialog.askopenfilename(initialdir = absPATH,filetypes = (("PNG","*.png"),("JPG","*.jpg"),("JPEG","*.jpeg"),("BMP","*.bmp")))
 
 img = cv2.imread(fullPATH)
-cv2.imshow("Engine",img)
+cv2.imshow("original image",img)
 
 # from colored to gray scaled color
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-cv2.imshow("grayscaled Engine",gray)
+cv2.imshow("grayscaled image",gray)
 
 # blur
 blur = cv2.GaussianBlur(img,(101,101),cv2.BORDER_DEFAULT) # ksize = (odd,odd)
-cv2.imshow("blurred Engine",blur)
+cv2.imshow("blurred image",blur)
 
-# Edges cascading
+# edges cascading
 cascade = cv2.Canny(img,125,175)
-cv2.imshow("edges Engine",cascade)
+cv2.imshow("edges image",cascade)
 
 # dialating img
-dialated = cv2.dilate(cascade,(3,3),iterations = 4)
-cv2.imshow("dialated Engine",dialated)
+dialated = cv2.dilate(cascade,(3,3),iterations=1)
+cv2.imshow("dialated image",dialated)
+
+# eroded  img
+eroded = cv2.erode(dialated,kernel=np.ones((1,1)),iterations=1)
+cv2.imshow("eroded image",eroded)
 
 cv2.waitKey()
 cv2.destroyAllWindows()
